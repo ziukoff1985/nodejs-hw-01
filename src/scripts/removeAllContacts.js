a634b769164e6263c62a90587d7b736426ddad30
Borys Ziukov <ziukoff1985@gmail.com>
2025-02-19 15:18:10 +0200

    changed info logs

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,9 +1,17 @@
 // Імпортуємо функцію 'writeContacts' для запису контактів у файл 'db.json'
+import { readContacts } from '../utils/readContacts.js';
 import { writeContacts } from '../utils/writeContacts.js';
 
 // Функція для видалення всіх контактів
 export const removeAllContacts = async () => {
   try {
+    // Читаємо контакти з файлу 'db.json'
+    const contacts = await readContacts();
+    // Якщо список контактів порожній - виводимо відповідне повідомлення
+    if (contacts.length === 0) {
+      console.log('❌ Список контактів порожній. Видалення неможливе.');
+      return; // припиняємо виконання функції
+    }
     // Записуємо порожній масив у файл 'db.json', тим самим видаляючи всі контакти
     await writeContacts([]);
     // Виводимо повідомлення про успішне видалення контактів
