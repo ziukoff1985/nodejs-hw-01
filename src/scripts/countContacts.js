4085509e78dbeb7aba1844246271d54d3b2805c2
Borys Ziukov <ziukoff1985@gmail.com>
2025-02-21 11:55:55 +0200

    added extra logs

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,11 +1,14 @@
 // Імпортуємо функцію 'readContacts' для читання контактів з файлу 'db.json'
 import { readContacts } from '../utils/readContacts.js';
+import path from 'node:path';
 
 // Функція для підрахунку загальної кількості контактів
 export const countContacts = async () => {
   try {
     // Читаємо контакти з файлу 'db.json'
     const contacts = await readContacts();
+    console.log(path.resolve('src', 'db', 'db.json'));
+    console.log(path.join(process.cwd(), 'src', 'db', 'db.json'));
     // Повертаємо повідомлення про загальну кількість контактів
     return `✅ Загальна кількість контактів - ${contacts.length}`;
   } catch (error) {
